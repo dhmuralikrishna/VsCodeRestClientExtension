b97900101835df47f0873507789622a5ec50d1e6
Sarayu <Sarayu@DESKTOP-0FQVAL8>
2018-10-04 23:55:05 -0400

    Added API

---
+++ Api/ProductApi/ProductApi/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApi.Data;
+using ProductApi.Models;
+using System.Collections.Generic;
+
+namespace ProductApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public IEnumerable<Product> Get()
+        {
+            return ProductRepository.Products; 
+        }
+
+        [HttpPost]
+        public int Post(Product product)
+        {
+            int count = ProductRepository.Products.Count;
+
+            ProductRepository.Products.Add(product);
+
+            return count + 1;
+        }
+
+    }
+}
---
+++ Api/ProductApi/ProductApi/Data/ProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProductApi.Models;
+
+namespace ProductApi.Data
+{
+    /// <summary>
+    ///  InMemory repository 
+    /// </summary>
+   static class ProductRepository
+    {
+        public static System.Collections.Generic.IList<Product> Products;
+    }
+}
